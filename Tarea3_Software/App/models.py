00fcc4d6604e59505ce9ecb56a70d111809fdc5e
Elvin Quero <elvinempor@gmail.com>
2018-05-16 23:01:36 -0400

    python3 no tiene unicode sino str.

--- Tarea3_Software/App/models.py
+++ Tarea3_Software/App/models.py
@@ -21,9 +21,9 @@ class Seguridad:
 		minus = 0
 		for i in range(len(key)): 
 			caracter = key[i]
-			if unicode.isdigit(caracter):
+			if str.isdigit(caracter):
 				digitos += 1
-			elif unicode.isalpha(caracter):
+			elif str.isalpha(caracter):
 				if caracter.isupper():
 					mayus += 1
 				else:
@@ -57,14 +57,14 @@ class Seguridad:
 
 		for i in range(len(seccion1)):
 			caracter = seccion1[i]
-			if ( (not unicode.isdigit(caracter)) and (not unicode.isalpha(caracter)) and (caracter != ".") and (caracter != "_") and (caracter != "-") ):
+			if ( (not str.isdigit(caracter)) and (not str.isalpha(caracter)) and (caracter != ".") and (caracter != "_") and (caracter != "-") ):
 				invalidos.append(caracter)
 
 		for i in range(len(seccionesPunto)):
 			n = len(seccionesPunto[i])
 			for j in range(n):
 				caracter = seccionesPunto[i][j]
-				if ( (not unicode.isdigit(caracter)) and (not unicode.isalpha(caracter))):
+				if ( (not str.isdigit(caracter)) and (not str.isalpha(caracter))):
 					invalidos.append(caracter)	
 		
 		if len(invalidos) != 2:
