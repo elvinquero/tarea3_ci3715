69ce3fda5231c707ccb9114215f2c9decf073802
elvinquero <401198+elvinquero@users.noreply.github.com>
2018-05-16 20:38:43 -0400

    Ya pasa los casos de prueba 5, 6 y 7

--- Tarea3_Software/App/models.py
+++ Tarea3_Software/App/models.py
@@ -13,13 +13,31 @@ class Seguridad:
 			asciiLetter = ord(firstKey[i]) + 3
 			newKey += chr(asciiLetter)
 		return newKey
+	
+	def verificarCorreo(self, mail):
+		invalidos = [1, ""]
 
+		seccionesArroba = mail.split("@")
+		if len(seccionesArroba) != 2:
+			if len(seccionesArroba) < 2:
+				return [0, "Correo electronico invalido: debe colocar @"]
+			else:
+				return [0, "Correo electronico invalido: no puede colocar mas de un @"]
+		else:
+			return invalidos
+	
 	def verificar(self, email, clave):
 		validMail = 1
 		mensajeMail = ""
 		validKey = 1
 		mensajeKey = ""
 		
+		charInvalidosMail = self.verificarCorreo(email)
+		if len(charInvalidosMail)==2:
+			if charInvalidosMail[0] < 1:
+				validMail = 0
+				mensajeMail = charInvalidosMail[1]	
+				
 		if len(clave) < 8 or len(clave) > 16:
 			validKey = 0
 			mensajeKey = "Clave invalida: debe tener entre 8 y 16 caracteres"
